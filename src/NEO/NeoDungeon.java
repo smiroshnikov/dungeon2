0c6379bc88a7ed7af204e8c32d111096c0bc922c
smiroshnikov <sergeim.qa@gmail.com>
2017-01-02 23:58:21 +0200

    Get back !

--- src/NEO/NeoDungeon.java
+++ src/NEO/NeoDungeon.java
@@ -1,8 +1,5 @@
 package NEO;
 
-/**
- * Created by sergei.miroshnikov on 31/12/2016.
- */
 public class NeoDungeon {
     public static void main(String[] args) {
         NeoMonster t = new NeoMonster(1);
